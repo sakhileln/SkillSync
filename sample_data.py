f73665e6b49e212e06d20c93c44908d01e125eb1
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-11 21:33:45 +0200

    Add sample response data.

---
+++ sample_data.py
@@ -0,0 +1,20 @@
+incorrect = {
+    'error': {'code': 400, 'message': 'INVALID_LOGIN_CREDENTIALS', 
+    'errors': [
+            {
+                'message': 'INVALID_LOGIN_CREDENTIALS', 
+                'domain': 'global', 
+                'reason': 'invalid'
+            }
+        ]
+    }
+}
+
+correct = {
+    'kind': 'identitytoolkit#VerifyPasswordResponse', 
+    'localId': 'MMASbsdjhAKSJnad7NBD', 
+    'email': 'sakhile@spacex.com', 
+    'displayName': '', 
+    'idToken': 'eyJhbGciKAJBDSMDNbsdkjdfhjALDJBHJSdksfJKV1QifQ.esdjhfdfbbvdbpwnBsTwxJXQ-CGPEdSup-XbMqlmcJTLVAJ7_jshfRWCJD6Q', 
+    'expiresIn': '3600'
+}
