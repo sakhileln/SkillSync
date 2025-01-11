7015b64bc3bdf7de15ac3eaee6a8dc0e08027d90
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-11 21:36:10 +0200

    Use 'match-case' statement to give user option to signin or signup.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""Main driver module."""
+
+from helper import sign_in, sign_up
+
 print("SkillSync")
 choice = input("Do you have an account [Y/n]: ")
 while choice not in ["", " ", "y", "Y", "n", "N"]:
@@ -8,7 +12,9 @@ while choice not in ["", " ", "y", "Y", "n", "N"]:
 match choice.lower():
     case "" | " " | "y":
         print("Login option")
+        sign_in()
     case "n":
         print("Sign up option")
+        sign_up()
     case _:
         print("Invalid choice")
