bc448f7d375897fb8946d337b46111fa8f9851bc
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-12 11:24:45 +0200

    Update CRUD operations.

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 import os
 
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import db, credentials
 
 from dotenv import load_dotenv
 
@@ -12,3 +12,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred, {"databaseURL": DATABASE_URL})
 
+# Creating reference to root node
+ref = db.reference("/")
+
+# Retrieving data from root node
+ref.get()
+db.reference("/name").get()
+
+# Set operation
+db.reference("/meetings").set(3)
+ref.get()
+
+# Update operation
+db.reference("/").update({"name": "Ndlazi"})
+ref.get()
