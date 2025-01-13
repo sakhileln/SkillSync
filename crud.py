2d5605cb123cf94ba034a94a2470d857ef43be00
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-13 23:14:15 +0200

    Update module.

--- crud.py
+++ crud.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Firebase configuration
+# configuration
 config = {
     "apiKey": os.getenv("FIREBASE_API_KEY"),
     "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
