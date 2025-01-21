fc1725d05a9494ae5fcbf09d9d1c8517f13702c9
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-21 20:11:04 +0200

    Update event retreival.

--- get_events.py
+++ get_events.py
@@ -17,7 +17,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
+    """
     Prints the start and name of the next 10 events on the user's calendar.
     """
     creds = None
