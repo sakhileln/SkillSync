c1581832e1e980b6653eca951426eba862b0de57
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-22 19:07:57 +0200

    Move 'get_events' to create_event module.

--- create_event.py
+++ create_event.py
@@ -5,10 +5,17 @@ start/end times, and invites a specified attendee. Authentication is managed usi
 OAuth 2.0, with credentials stored for subsequent runs.
 """
 
+import datetime
 import os
+import os.path
 import pickle
 
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from termcolor import cprint
@@ -76,6 +83,39 @@ def create_event(email):
     )
 
 
+def get_events():
+    try:
+        service = build("calendar", "v3", credentials=authenticate_google())
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
 def delete_event(event_id):
     """Delete a specific event by its Google Calendar event ID"""
     try:
@@ -104,4 +144,5 @@ if __name__ == "__main__":
     # Test with one email
     # create_event("sakhilelindah@gmail.com")
     # delete_event("Quick SkillSync Meeting")
-    print(get_event_id(event_summary="Quick SkillSync Meeting"))
+    # print(get_event_id(event_summary="Quick SkillSync Meeting"))
+    get_events()
