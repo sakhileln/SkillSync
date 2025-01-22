81b7ad80050f0137e512d52b76f76bc8c9e5cf73
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-22 19:54:48 +0200

    Fix [W0611]: Unused Credentials imported from 'google.oauth2.credentials'.

--- events.py
+++ events.py
@@ -12,7 +12,6 @@ import pickle
 
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
 from termcolor import cprint
