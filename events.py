e2bd65feaea67e284869ea26ce53d3845e946ee0
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-22 19:52:08 +0200

    Fix [W0404]: Reimport 'build'.

--- events.py
+++ events.py
@@ -14,7 +14,6 @@ from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
