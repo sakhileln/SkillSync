712919573990b046ec03d77bbcca8e18fcc9aa4c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-22 19:52:55 +0200

    Fix [W0404]: Reimport 'InstalledAppFlow'.

--- events.py
+++ events.py
@@ -15,7 +15,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from termcolor import cprint
 
